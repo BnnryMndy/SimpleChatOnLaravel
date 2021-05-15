d05b43998deddf80247020adbdaee9f8a0a6b766
AntonLazarev <bnnrymndy@gmail.com>
2021-05-15 12:55:02 +0300

    added blank views for app

--- SimpleChat/routes/web.php
+++ SimpleChat/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/messages', function () {
+    return view('messages');
 });
